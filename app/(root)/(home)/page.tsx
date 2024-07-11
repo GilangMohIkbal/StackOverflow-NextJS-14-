d8e532d3fecc93068b643bb57bea3f3bcde08f80
muhammad iqbal <gilangmohikbal@gmail.com>
2024-07-11 13:06:00 +0700

    set up auth with clerk

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+export default function Home() {
+  return (
+    <div>
+      <h1 className="h1-bold">Nextjs 14 we&apos;re coming !!</h1>
+      <UserButton />
+    </div>
+  );
+}
