86209b8fe7940a7ae807c759ed8445d0b3e2941e
muhammad iqbal <gilangmohikbal@gmail.com>
2024-07-11 21:32:41 +0700

    implemented theme provider and change logo favicon

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,10 @@
 import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
       <h1 className="h1-bold">Nextjs 14 we&apos;re coming !!</h1>
+
       <UserButton />
     </div>
   );
